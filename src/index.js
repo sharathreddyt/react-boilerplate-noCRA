21091fa8c87f66a0558896a9586304fb7e5bd54e
sharath <sharath.theegulla@optum.com>
2018-11-24 20:01:55 -0600

    eslint added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-
-import  React  from "react";
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 const Index = () => {
     return <div>Hello sharath!!!</div>
 }
 
-ReactDOM.render(<Index/>,document.getElementById("index"));
+ReactDOM.render(<Index />, document.getElementById('index'))
